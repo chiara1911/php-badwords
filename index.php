0c12f5c3f371b77ee5056a6b8dd9289b5582404e
chiara1911 <chiaracesari89@gmail.com>
2023-11-27 15:39:25 +0100

    add form in index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+
+<?php
+$name= $_GET["paragraph"];
+$badwords= $_GET["badwords"];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +13,17 @@
     <title>Php Badwords</title>
 </head>
 <body>
+<header></header>
+    <main>
+      
+        <form action="index.php" method="GET">
+    <input type="text" name="paragraph">
+    <input type="text" name="badwords">
+    <button type="submit">Invia</button>
+    <h1> <?php echo "$name $badwords" ?></h1>
+ </form>
+    </main>
+    <footer></footer>
     
 </body>
 </html>
